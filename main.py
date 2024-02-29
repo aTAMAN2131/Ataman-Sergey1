b1f8c5c521a4237af1bcb21b2d80ca6f236808cb
aTAMAN2131 <161508149+aTAMAN2131@users.noreply.github.com>
2024-02-29 10:33:23 +0000

    add code Pyhone 2

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-def main():
-  N = int(input("Введіть бажаний розмір матриці: "))
-  A = [[0] * N for _ in range(N)]
-  sum_val = 0
-  positive_count = 0
-  print("Введіть елементи матриці {}x{}:".format(N, N))
-  for i in range(N):
-      row_input = input().split() 
-      for j in range(N):
-          A[i][j] = int(row_input[j])  
-          if i < j and A[i][j] > 0:
-              sum_val += A[i][j]
-              positive_count += 1
-  print("Сума додатніх елементів над головною діагоналлю:", sum_val)
-  print("Кількість додатніх елементів над головною діагоналлю:", positive_count)
-if __name__ == "__main__":
-  main()
+import math
+def calculate_distance(x1, y1, x2, y2):
+    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+def calculate_triangle_area(x1, y1, x2, y2, x3, y3):
+    side1 = calculate_distance(x1, y1, x2, y2)
+    side2 = calculate_distance(x2, y2, x3, y3)
+    side3 = calculate_distance(x3, y3, x1, y1)
+    s = (side1 + side2 + side3) / 2
+    area = math.sqrt(s * (s - side1) * (s - side2) * (s - side3))
+    if area <= 0:
+        print("Трикутник не існує, оскільки площа менше або дорівнює нулю.")
+        return 0
+    return area
+x1 = float(input("Введіть x-координату першої вершини: "))
+y1 = float(input("Введіть y-координату першої вершини: "))
+x2 = float(input("Введіть x-координату другої вершини: "))
+y2 = float(input("Введіть y-координату другої вершини: "))
+x3 = float(input("Введіть x-координату третьої вершини: "))
+y3 = float(input("Введіть y-координату третьої вершини: "))
+triangle_area = calculate_triangle_area(x1, y1, x2, y2, x3, y3)
+if triangle_area > 0:
+    print("Площа трикутника з заданими координатами вершин: ", triangle_area)
+
